4fd3761e4f6252c472d0c017841ac76440f19333
Reecha Shrestha <45999571+reechashrestha@users.noreply.github.com>
2020-05-22 10:51:56 +0545

    Roles Added

---
+++ CourseWork/CourseWork2/Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using CourseWork2.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseWork2.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+
+        }
+
+        public ActionResult Index()
+        {
+            var Role =context.Roles.ToList();
+            return View(Role);
+        }
+        // GET: Role
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
