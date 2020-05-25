4732a17a315c75cc3ea083abaaadb177394b7361
Shubi-7 <45991712+Shubi-7@users.noreply.github.com>
2020-05-25 14:34:06 +0545

    Abum Artist Models Added

--- CourseWork/CourseWork2/Models/Artist.cs
+++ CourseWork/CourseWork2/Models/Artist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,16 @@ namespace CourseWork2.Models
 {
     public class Artist
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(55)]
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public ICollection<Album> Album { get; set; }
+
     }
 }
